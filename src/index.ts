e4687782e21bd8e08f8c9df3378037d4f6396575
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 20:12:23 +0700

    add documentations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// index.ts
-
 import express from "express";
 import createError from "http-errors";
 import carRoutes from "../routes/carRoutes";
@@ -12,23 +10,26 @@ import path from "path";
 
 const app = express();
 
+// Middleware to parse JSON and enable CORS
 app.use(express.json(), cors());
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 
-// Use the carRoutes module for the '/cars' route
+// Use the routes modules
 app.use('/cars', carRoutes);
 app.use('/customers', customerRoutes);
 app.use('/orders', orderRoutes);
 app.use('/trucks', truckRoutes);
 app.use('/motorcycles', motorcycleRoutes);
 
-// handle 404 error
+// Handle 404 error
 app.use((req, res, next) => {
     next(createError(404));
 });
 
+// Start the server on port 3000
 app.listen(3000, () =>
     console.log(`⚡️[server]: Server is running at https://localhost:3000`)
 );
