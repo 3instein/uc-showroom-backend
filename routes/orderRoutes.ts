e6af0bad36a1ac66465857eae322cd2ee8547f55
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 12:32:33 +0700

    Refactor orderRoutes to include vehicle_type in response

--- routes/orderRoutes.ts
+++ routes/orderRoutes.ts
@@ -11,28 +11,35 @@ router.get('/', async (req: Request, res: Response) => {
         const carOrders = await prisma.carOrder.findMany({
             include: {
                 car: true,
-                customer: true
-            }
+                customer: true,
+            },
         });
+        
         const truckOrders = await prisma.truckOrder.findMany({
             include: {
                 truck: true,
-                customer: true
-            }
+                customer: true,
+            },
         });
+        
         const motorcycleOrders = await prisma.motorcycleOrder.findMany({
             include: {
                 motorcycle: true,
-                customer: true
-            }
+                customer: true,
+            },
         });
-
-        const orders = [...carOrders, ...truckOrders, ...motorcycleOrders];
-
+        
+        // Convert each order to a common structure with vehicle_type
+        const ordersWithVehicleType = [
+            ...carOrders.map(order => ({ ...order, vehicle_type: 'car' })),
+            ...truckOrders.map(order => ({ ...order, vehicle_type: 'truck' })),
+            ...motorcycleOrders.map(order => ({ ...order, vehicle_type: 'motorcycle' })),
+        ];
+        
         res.json({
             success: true,
-            data: orders
-        });
+            data: ordersWithVehicleType,
+        });        
     } catch (error) {
         console.error("Error retrieving orders:", error);
         res.status(500).json({
