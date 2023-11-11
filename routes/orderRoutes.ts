561482034d0dbbb4f7fd1ddf9c5aa1d6dfd4dc8c
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 12:51:13 +0700

    Add delete routes for car, truck, and motorcycle orders

--- routes/orderRoutes.ts
+++ routes/orderRoutes.ts
@@ -14,34 +14,103 @@ router.get('/', async (req: Request, res: Response) => {
                 customer: true,
             },
         });
-        
+
         const truckOrders = await prisma.truckOrder.findMany({
             include: {
                 truck: true,
                 customer: true,
             },
         });
-        
+
         const motorcycleOrders = await prisma.motorcycleOrder.findMany({
             include: {
                 motorcycle: true,
                 customer: true,
             },
         });
-        
+
         // Convert each order to a common structure with vehicle_type
         const ordersWithVehicleType = [
             ...carOrders.map(order => ({ ...order, vehicle_type: 'car', vehicle_price: order.car.price })),
             ...truckOrders.map(order => ({ ...order, vehicle_type: 'truck', vehicle_price: order.truck.price })),
             ...motorcycleOrders.map(order => ({ ...order, vehicle_type: 'motorcycle', vehicle_price: order.motorcycle.price })),
         ];
-        
+
         res.json({
             success: true,
             data: ordersWithVehicleType,
-        });        
+        });
     } catch (error) {
         console.error("Error retrieving orders:", error);
+        res.status(500).json({
+            success: false,
+            error: "Internal Server Error"
+        });
+    }
+})
+
+router.delete('/cars/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const order = await prisma.carOrder.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.json(
+            {
+                success: true,
+                data: order
+            }
+        );
+    } catch (error) {
+        console.error("Error deleting order:", error);
+        res.status(500).json({
+            success: false,
+            error: "Internal Server Error"
+        });
+    }
+})
+
+router.delete('/trucks/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const order = await prisma.truckOrder.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.json(
+            {
+                success: true,
+                data: order
+            }
+        );
+    } catch (error) {
+        console.error("Error deleting order:", error);
+        res.status(500).json({
+            success: false,
+            error: "Internal Server Error"
+        });
+    }
+})
+
+router.delete('/motorcycles/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const order = await prisma.motorcycleOrder.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.json(
+            {
+                success: true,
+                data: order
+            }
+        );
+    } catch (error) {
+        console.error("Error deleting order:", error);
         res.status(500).json({
             success: false,
             error: "Internal Server Error"
